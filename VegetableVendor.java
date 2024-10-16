f6ea54833ecfe9c46319404ddcac58e91a3a6282
zephydrbl90 <readyin69321@gmail.com>
2024-10-16 08:31:19 +0700

    VegetableVendor.java

--- VegetableVendor.java
+++ VegetableVendor.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class FruitVendor {
+public class VegetableVendor {
     public static void main (String[]args){
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("What do you want to buy?");
-        System.out.println("1. Apple\n2. Manggo\n3. Orange\ninput:");
-        int choice21 = input.nextInt();
+        System.out.println("1. Broccoli\n2. Pea\n3. Onion\ninput:");
+        int choice31 = input.nextInt();
 
         System.out.println("How much?\ninput:");
-        int choice22 = input.nextInt();
+        int choice32 = input.nextInt();
 
         System.out.println("where do you go next?\n1. Next Shop\n2. Go Home\ninput:");
-        int choice23 = input.nextInt();
+        int choice33 = input.nextInt();
     }
 }
-
